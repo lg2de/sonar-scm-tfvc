0cd6310398170862b02e9690785c33c8f797edbc
Saleha Hussain <salehahu@microsoft.com>
2015-09-02 13:39:33 +0530

    enabled suppirt for tfs15 dlls

---
+++ SonarTfsAnnotate/AssemblyHelper.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * SonarQube :: SCM :: TFVC :: Plugin
+ * Copyright (c) SonarSource SA and Microsoft Corporation.  All rights reserved.
+ *
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+using System.Reflection;
+
+namespace SonarSource.TfsAnnotate
+{
+    public class AssemblyHelper
+    {
+        public virtual Assembly LoadAssemblyFromGAC(string parsedAssemblyName)
+        {
+            return Assembly.Load(parsedAssemblyName);
+        }
+
+        public virtual bool CheckAssemblyInGAC(string parsedAssemblyName)
+        {
+            return Assembly.ReflectionOnlyLoad(parsedAssemblyName).GlobalAssemblyCache;
+        }
+
+        public virtual Assembly LoadAssembly(string assemblyPath)
+        {
+            return Assembly.LoadFrom(assemblyPath);
+        }
+    }
+}
