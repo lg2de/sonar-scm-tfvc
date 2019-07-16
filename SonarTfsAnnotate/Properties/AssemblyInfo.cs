041da64f28144073024cbfe7ec9363dcd67a0861
Lukas Grützmacher <github@lg2.de>
2019-07-16 15:54:12 +0200

    prepare release 2.2

--- SonarTfsAnnotate/Properties/AssemblyInfo.cs
+++ SonarTfsAnnotate/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * SonarQube :: SCM :: TFVC :: Plugin
  * Copyright (c) SonarSource SA and Microsoft Corporation.  All rights reserved.
  *
@@ -7,7 +7,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SonarTfsAnnotate")]
@@ -15,26 +15,16 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SonarSource")]
 [assembly: AssemblyProduct("SonarTfsAnnotate")]
-[assembly: AssemblyCopyright("Copyright © SonarSource 2014")]
+[assembly: AssemblyCopyright("Copyright � SonarSource 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ef935e23-3dbf-401d-a5c3-62b3ec660a46")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.2.0.0")]
