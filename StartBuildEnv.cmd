e7fd777475c3416b1cf9df5e49938cda21c92f55
Lukas Grützmacher <44983012+lg2de@users.noreply.github.com>
2021-04-06 23:24:52 +0200

    activate code coverage with JaCoCo (#63)

--- StartBuildEnv.cmd
+++ StartBuildEnv.cmd
@@ -1,3 +1,4 @@
-set JAVA_HOME=c:\Program Files\Java\jdk1.8.0_211
+set JAVA_HOME=c:\Program Files\OpenJDK\openjdk-11.0.10_9\
+set PATH=%JAVA_HOME%\bin;%PATH%
 set MSBUILD_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe
 cmd.exe
