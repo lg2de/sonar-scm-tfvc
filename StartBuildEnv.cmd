0b610ae2e5e14bf585d1a8cbd19816e4873e45be
Lukas Grützmacher <44983012+lg2de@users.noreply.github.com>
2023-11-30 16:34:24 +0100

    Update maven and jdk 17 (#76)

--- StartBuildEnv.cmd
+++ StartBuildEnv.cmd
@@ -1,4 +1,3 @@
-set JAVA_HOME=c:\Program Files\OpenJDK\openjdk-11.0.15_10\
 set PATH=%JAVA_HOME%\bin;%PATH%
 set MSBUILD_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe
 cmd.exe
