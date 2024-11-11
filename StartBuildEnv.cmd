3a48c88d2ff2073b9d5680644912f6fb211f2c34
Lukas Grützmacher <44983012+lg2de@users.noreply.github.com>
2024-11-11 18:18:32 +0100

    Run dotnet tests (#116)

--- StartBuildEnv.cmd
+++ StartBuildEnv.cmd
@@ -1,3 +1,4 @@
 set PATH=%JAVA_HOME%\bin;%PATH%
 set MSBUILD_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe
+set MSTEST_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\Extensions\TestPlatform\vstest.console.exe
 cmd.exe
